72b134a04e181130cd7227c403242f689180b7c2
EnesAlgul <86858992+Phenomex@users.noreply.github.com>
2021-09-05 22:04:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+lyear = int(input("Please enter last year month's revenue.\n"))
+tyear = int(input("Please enter last year month's revenue.\n"))
+differ = "The difference is $"
+
+def hesapla():
+  print(f" {differ} {lyear - tyear}")
+  
+
+hesapla()
+
+def sonuc():
+  print(f",{df} {goal / days_left / adr}{rooms}")
+
+goal = int(input("How much would you like to make this month? "))
+adr = float(input("What is your average daily rate? "))
+days_left = int(input("How many days left to the end of month? "))
+df = "You should sell about "
+rooms = "rooms per day."
+
+sonuc()
+
+  
+
+
+
+
